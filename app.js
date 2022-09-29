77e1f49020ed3b5412cbefc90ef95445c565a051
Zain-Ashfaq <zashfaq00@gmail.com>
2022-09-29 16:49:47 +0100

    display screen gets auto updated with button press

--- app.js
+++ app.js
@@ -9,15 +9,33 @@
 // button should also push operators? how to seperate the operator and number in an array?
 
 const displayScreenArray = [];
+const screen = document.querySelector(".show_screen");
 
 const number_0 = document.querySelector("#number-0");
+const number_1 = document.querySelector("#number-1");
 
 // console.log(number_0);
 
 const addNumberFunction = (event) => {
+  //
   // alert("button 0 has been pressed");
+
   displayScreenArray.push(Number(number_0.textContent));
   console.log(displayScreenArray);
+  screen.innerText = displayScreenArray;
+
+  updateScreen();
+};
+
+const updateScreen = () => {
+  screen.innerText = displayScreenArray;
 };
 
 number_0.addEventListener("click", addNumberFunction);
+number_1.addEventListener("click", addNumberFunction);
+
+// screen.textContent = "test change screen";
+
+// screen.addEventListener("change", updateScreen);
+
+console.log(screen.innerText);
